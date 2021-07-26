b14beaa19057bebbd99f6314c792de12dc195ae7
melissa.muiru@student.moringaschool.com <melissa.muiru@student.moringaschool.com>
2021-07-26 12:03:59 +0300

    add delanistudio.html

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,1 @@
-//create variables
-        <h5>WHAT WE DO</h5>
-        $('para1').hide();
-        const toggledesign_icon = document.querySelector('#para1');
-        const divdesign_icon = document.querySelector('design_icon');
-
-        toggledesign_icon.addEventListener('click',()=>{
-            if(divdesign_icon.getElementsByClassName.display==='none')(
-                divdesign_icon.getElementsByClassName.display='block';
-                toggledesign_icon.innerHTML='hide para1'
-            }else{
-
-            }
+$('p#para1').hide();
