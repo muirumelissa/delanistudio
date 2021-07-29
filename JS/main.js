6ad5227a5f3ddf2a829e4cce38e434be9383e995
melissa.muiru@student.moringaschool.com <melissa.muiru@student.moringaschool.com>
2021-07-29 14:21:36 +0300

    add delanistudio.html

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('#design_icon').ready(function(){
+        $('.para1').hide();
+    });
+    $('#development').ready(function(){
+        $('.para2').hide();
+    });
+    $('#product').ready(function(){
+        $('.para3').hide();
+    });
+    $('#design_icon').click(function(){
+       $('.para1').toggle();
+     });
+
+     $('#development').click(function(){
+         $('.para2').toggle();
+     });
+
+     $('#product').click(function(){
+         $('.para3').toggle();
+     });
+ });
+
